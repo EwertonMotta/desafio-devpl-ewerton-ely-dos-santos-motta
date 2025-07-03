4c67e34096a8163b02a4920b33bf6798f08a9ebe
Ewerton Motta <contato@ewerton.dev>
2025-07-03 10:37:57 -0300

    refactor: garantir que tarefas concluídas tenham data de conclusão

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -10,13 +10,15 @@ class TaskFactory extends Factory
 {
     public function definition(): array
     {
+        $completed = $this->faker->boolean();
+
         return [
             'user_id'      => $this->faker->numberBetween(1, 10), // Assuming you have 10 users seeded
             'title'        => $this->faker->sentence(),
             'description'  => $this->faker->paragraph(),
             'deadline'     => $this->faker->dateTimeBetween('-1 month', '+1 month'),
-            'completed'    => $this->faker->boolean(),
-            'completed_at' => $this->faker->optional()->dateTimeBetween('-1 month', 'now'),
+            'completed'    => $completed,
+            'completed_at' => $completed ? $this->faker->dateTimeBetween('-1 month', 'now') : null,
             'created_at'   => $this->faker->dateTimeBetween('-1 month', 'now'),
             'updated_at'   => $this->faker->dateTimeBetween('-1 month', 'now'),
         ];
