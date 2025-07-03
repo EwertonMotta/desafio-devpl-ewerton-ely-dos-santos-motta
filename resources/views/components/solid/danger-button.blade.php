b2fff33ffa12cb17fed1500f87252526d6740507
Ewerton Motta <contato@ewerton.dev>
2025-07-02 21:10:19 -0300

    feat: adicionar novos componentes de botão e modal

--- resources/views/components/solid/danger-button.blade.php
+++ resources/views/components/solid/danger-button.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $class =
-        'cursor-pointer rounded-lg border border-blue-700 px-5 py-2.5 text-center text-sm font-medium text-blue-700 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white dark:focus:ring-blue-800';
+        'flex items-center gap-2 cursor-pointer focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900';
 @endphp
 
 @if (in_array($type, ['button', 'submit']))
--- resources/views/components/solid/primary-button.blade.php
+++ resources/views/components/solid/primary-button.blade.php
@@ -2,7 +2,7 @@
 
 @php
     $class =
-        'cursor-pointer rounded-lg border border-blue-700 px-5 py-2.5 text-center text-sm font-medium text-blue-700 hover:bg-blue-800 hover:text-white focus:outline-none focus:ring-4 focus:ring-blue-300 dark:border-blue-500 dark:text-blue-500 dark:hover:bg-blue-500 dark:hover:text-white dark:focus:ring-blue-800';
+        'flex items-center gap-2 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800';
 @endphp
 
 @if (in_array($type, ['button', 'submit']))
