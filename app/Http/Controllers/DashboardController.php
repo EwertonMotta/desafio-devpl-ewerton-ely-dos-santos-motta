364750b29f14ba077084c1c8277ff994b856909c
Ewerton Motta <contato@ewerton.dev>
2025-07-02 21:13:01 -0300

    feat: atualizar o dashboard com contagem de tarefas e exibição de dados mensais

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,11 +6,68 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Request $request)
     {
-        return view('dashboard');
+        $tasks = auth()->user()->tasks()
+        ->where('completed', false)
+        ->orderBy('created_at', 'asc')
+        ->limit(5)
+        ->get();
+
+        $createdCurrentMonth = auth()->user()->tasks()
+        ->whereMonth('created_at', now()->month)
+        ->whereYear('created_at', now()->year)
+        ->count();
+
+        $createdLastMonth = auth()->user()->tasks()
+            ->whereMonth('created_at', now()->subMonth())
+            ->whereYear('created_at', now()->year)
+            ->count();
+
+        $completedCurrentMonth = auth()->user()->tasks()
+            ->whereMonth('completed_at', now()->month)
+            ->whereYear('completed_at', now()->year)->count();
+
+        $completedLastMonth = auth()->user()->tasks()
+            ->whereMonth('completed_at', now()->subMonth())
+            ->whereYear('completed_at', now()->year)
+            ->count();
+
+        $createdCompletedCurrentMonth = auth()->user()->tasks()
+            ->whereMonth('created_at', now()->month)
+            ->whereYear('created_at', now()->year)
+            ->whereMonth('completed_at', now()->month)
+            ->whereYear('completed_at', now()->year)
+            ->count();
+        $createdCompletedLastMonth = auth()->user()->tasks()
+            ->whereMonth('created_at', now()->subMonth())
+            ->whereYear('created_at', now()->year)
+            ->whereMonth('completed_at', now()->subMonth())
+            ->whereYear('completed_at', now()->year)
+            ->count();
+
+        $createdPendingCurrentMonth = auth()->user()->tasks()
+            ->whereMonth('created_at', now()->month)
+            ->whereYear('created_at', now()->year)
+            ->where('completed', false)
+            ->count();
+
+        $createdPendingLastMonth = auth()->user()->tasks()
+            ->whereMonth('created_at', now()->subMonth())
+            ->whereYear('created_at', now()->year)
+            ->where('completed', false)
+            ->count();
+
+        return view('dashboard', compact(
+            'tasks',
+            'createdCurrentMonth',
+            'createdLastMonth',
+            'completedCurrentMonth',
+            'completedLastMonth',
+            'createdCompletedCurrentMonth',
+            'createdCompletedLastMonth',
+            'createdPendingCurrentMonth',
+            'createdPendingLastMonth'
+        ));
     }
 }
